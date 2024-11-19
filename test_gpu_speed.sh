d926f82836b885ea06f3042ed04d50ba0964d4ac
KAIwangke <qqtenxun1234@gmail.com>
2024-11-19 22:28:22 +0000

    remove

--- test_gpu_speed.sh
+++ test_gpu_speed.sh
@@ -17,26 +17,12 @@ echo "----------------------------------------"
 IMPLEMENTATIONS=("stage0" "stage1" "stage2" "stage3" "cufft")
 
 for impl in "${IMPLEMENTATIONS[@]}"; do
-    echo -n "$impl: "
-    total=0
-    valid_runs=0
-
+    echo "Running $impl implementation:"
+    
     # Run implementation multiple times
     for ((i=1; i<=RUNS; i++)); do
-        output=$(./fft_${impl} "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat" 2>&1)
-        # Extract the numeric execution time from the output
-        time=$(echo "$output" | grep "Total execution time:" | awk '{print $(NF-1)}')
-
-        if [ ! -z "$time" ]; then
-            total=$(echo "$total + $time" | bc)
-            valid_runs=$((valid_runs + 1))
-        fi
+        echo "Run $i:"
+        ./fft_${impl} "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat"
+        echo "----------------------------------------"
     done
-
-    if [ $valid_runs -gt 0 ]; then
-        average=$(echo "scale=4; $total / $valid_runs" | bc)
-        echo "$average ms"
-    else
-        echo "Failed to get timing"
-    fi
 done
