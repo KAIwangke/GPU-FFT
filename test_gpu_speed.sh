c0d6725b34b0f1dfc0734dcd75f8ec14bb7b3cb4
KAIwangke <qqtenxun1234@gmail.com>
2024-11-19 22:23:17 +0000

    modi

--- test_gpu_speed.sh
+++ test_gpu_speed.sh
@@ -3,43 +3,40 @@
 MATRIX_SIZE=1024
 RUNS=5
 
-# First ensure input directory exists
-mkdir -p ../input
-
-# Array of GPU implementations to test
-IMPLEMENTATIONS=("stage0" "stage1" "stage2" "stage3" "cufft")
+# Check for input matrix
+if [ ! -f "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat" ]; then
+    echo "Generating input matrix..."
+    ./generate_matrix $MATRIX_SIZE ..
+else
+    echo "Using existing input matrix"
+fi
 
 echo "Testing GPU FFT implementations on ${MATRIX_SIZE}x${MATRIX_SIZE} matrix ($RUNS runs each)"
 echo "----------------------------------------"
 
-# First verify input file exists
-if [ ! -f "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat" ]; then
-    echo "Generating input matrix..."
-    ./generate_matrix $MATRIX_SIZE ..
-fi
+IMPLEMENTATIONS=("stage0" "stage1" "stage2" "stage3" "cufft")
 
 for impl in "${IMPLEMENTATIONS[@]}"; do
     echo -n "$impl: "
-    
-    # Run and collect times
     total=0
     valid_runs=0
     
+    # Run implementation multiple times
     for ((i=1; i<=RUNS; i++)); do
-        # Note: Changed to look for "Total execution time:" and "ms"
-        time=$(./fft_${impl} "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat" 2>&1 | grep "Total execution time:" | awk '{print $4}')
+        output=$(./fft_${impl} "../input/${MATRIX_SIZE}x${MATRIX_SIZE}_matrix.dat" 2>&1)
+        # Extract just the number from "Stage0: Total execution time: 11.4477 ms"
+        time=$(echo "$output" | grep "Total execution time:" | cut -d' ' -f4)
         
         if [ ! -z "$time" ]; then
-            total=$(echo "$total + $time" | bc -l)
+            total=$(echo "$total + $time" | bc)
             valid_runs=$((valid_runs + 1))
         fi
     done
     
-    # Calculate and print average if we have valid runs
     if [ $valid_runs -gt 0 ]; then
-        average=$(echo "scale=6; $total / $valid_runs" | bc -l)
+        average=$(echo "scale=4; $total / $valid_runs" | bc)
         echo "$average ms"
     else
-        echo "Failed to get valid timing"
+        echo "Failed to get timing"
     fi
 done
